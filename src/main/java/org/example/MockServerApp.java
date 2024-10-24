908c637de5fba4346c418415a485d690599e8947
Joshua Lindow <joshualindow@Joshuas-MacBook-Pro.local>
2024-10-23 20:38:04 -0600

    Update POST expectation to store passed in key-value pairs in a map.

--- src/main/java/org/example/MockServerApp.java
+++ src/main/java/org/example/MockServerApp.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mockserver.integration.ClientAndServer;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,7 +13,9 @@ import static org.mockserver.model.HttpResponse.response;
 
 public class MockServerApp {
     private static ClientAndServer mockServer;
-    private Map<String, String> memory = new HashMap<>();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static Map<String, String> memory = new HashMap<>();
+
 
     public static void main(String[] args) {
         mockServer = ClientAndServer.startClientAndServer(1080);
@@ -36,13 +41,24 @@ public class MockServerApp {
                         request()
                                 .withMethod("POST")
                                 .withPath("/api/example")
-                                .withBody("{\"key\":\"value\"}")
                 )
-                .respond(
-                        response()
-                                .withStatusCode(200)
-                                .withBody("{\"message\": \"Received: {\\\"key\\\":\\\"value\\\"}\"}")
-                                .withHeader("Content-Type", "application/json")
+                .respond( request -> {
+                        try {
+                            JsonNode jsonNode = objectMapper.readTree(request.getBodyAsString());
+                            jsonNode.fieldNames().forEachRemaining(field -> {
+                                String value = jsonNode.get(field).asText();
+                                memory.put(field, value);
+                            });
+                            System.out.println(memory.toString());
+                            return response()
+                                    .withStatusCode(200)
+                                    .withBody("Added to memory");
+                        } catch (IOException e){
+                            return response()
+                                    .withStatusCode(500)
+                                    .withBody("Failed with errorMessage: " + e.getMessage());
+                        }
+                    }
                 );
 
     }
